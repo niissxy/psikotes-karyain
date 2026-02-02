98c32dd55db7b72d41ce92ff04ee06f22aa904b3
niissxy <anisay106@gmail.com>
2026-02-02 14:40:24 +0700

    fix redirect and layout

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -5,35 +5,30 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Swal from "sweetalert2";
 
+export const dynamic = "force-dynamic";
+
 export default function RedirectPage() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const router = useRouter();
 
   useEffect(() => {
-    if (status === "loading") {
-      Swal.fire({
-        title: "Loading...",
-        allowOutsideClick: false,
-        didOpen: () => {
-          Swal.showLoading();
-        },
-      });
-      return;
-    }
-
-    Swal.close();
-
     if (!session) return;
+
+    Swal.fire({
+      title: "Loading...",
+      allowOutsideClick: false,
+      didOpen: () => Swal.showLoading(),
+    });
 
     if (session.user.role === "ADMIN") {
       router.push("/admin");
     } else {
       router.push("/soal");
     }
-  }, [session, status, router]);
+  }, [session, router]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="min-h-screen flex items-center justify-center">
       <p>Redirecting...</p>
     </div>
   );
