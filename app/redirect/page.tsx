f8327d6c3a1314161cd3ecef375cd35309b87b8a
niissxy <anisay106@gmail.com>
2026-02-02 10:43:07 +0700

    update redirect

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -6,24 +6,35 @@ import { useEffect } from "react";
 import Swal from "sweetalert2";
 
 export default function RedirectPage() {
-    const { data: session } = useSession();
-    const router = useRouter();
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-    useEffect (() => {
-        if (!session) return;
+  useEffect(() => {
+    if (status === "loading") {
+      Swal.fire({
+        title: "Loading...",
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+      return;
+    }
 
-        if(session.user.role === "ADMIN") {
-            router.push("/admin");
-        } else {
-            router.push("/soal");
-        }
-    }, [session]);
+    Swal.close();
 
-    return Swal.fire({
-            title: "Loading...",
-            allowOutsideClick: false,
-            didOpen: () => {
-            Swal.showLoading();
-          },
-        });
+    if (!session) return;
+
+    if (session.user.role === "ADMIN") {
+      router.push("/admin");
+    } else {
+      router.push("/soal");
+    }
+  }, [session, status, router]);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p>Redirecting...</p>
+    </div>
+  );
 }
