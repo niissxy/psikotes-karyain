8ff5cd009a68b74d10b4f0680967f633fdd4b9e3
niissxy <anisay106@gmail.com>
2026-02-02 11:15:47 +0700

    add type routes id

--- app/api/admin/peserta/[id]/route.ts
+++ app/api/admin/peserta/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-import { Prisma } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 
 type JawabanWithSoal = Prisma.JawabanGetPayload<{
   include: {
