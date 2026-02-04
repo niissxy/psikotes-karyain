fd878a3ac7342bf38fdd23e5d2cf7dd3242de4cc
niissxy <anisay106@gmail.com>
2026-02-04 10:25:25 +0700

    change posisi (admin page)

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -229,7 +229,6 @@ const nilaiAkhir = ((totalSkor / totalSoal) * 100).toFixed(2);
             <p><b>Jenis Kelamin <span style={{ marginLeft: 13 }}>: </span></b> {selectedPeserta.jenis_kelamin}</p>
             <p><b>Pendidikan <span style={{ marginLeft: 31 }}>: </span></b> {selectedPeserta.tingkat_pendidikan}</p>
             <p><b>Instansi <span style={{ marginLeft: 57 }}>: </span></b> {selectedPeserta.instansi}</p>
-            <p><b>Posisi <span style={{ marginLeft: 57 }}>: </span></b> {selectedPeserta.posisi}</p>
             <p><b>Kontak <span style={{ marginLeft: 62 }}>: </span></b> {selectedPeserta.kontak}</p>
               {selectedPeserta.portofolio && ( 
               <p><b>Portofolio <span style={{ marginLeft: 43 }}>: </span></b> 
@@ -242,6 +241,7 @@ const nilaiAkhir = ((totalSkor / totalSoal) * 100).toFixed(2);
               </a>
 
               </p> )}
+              <p><b>Posisi <span style={{ marginLeft: 57 }}>: </span></b> {selectedPeserta.posisi}</p>
           </div>
         </div>
       )}
