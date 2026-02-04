6e69af3cebeda01b180ba0a8ec80af40efc5ceae
niissxy <anisay106@gmail.com>
2026-02-04 13:57:42 +0700

    update instansi

--- app/soal/page.tsx
+++ app/soal/page.tsx
@@ -286,7 +286,7 @@ export default function SoalPage() {
             </div>
 
             <div className="mb-2">
-              <p>Instansi</p>
+              <p>Instansi yang dilamar</p>
               <input
                 className="w-full mb-3 px-4 py-2 border border-gray-300 rounded-md" 
                 type="text" 
