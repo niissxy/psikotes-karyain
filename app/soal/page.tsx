b5853a5f6f63e069036a6ecef6b6515c67aa5fa5
niissxy <anisay106@gmail.com>
2026-02-06 13:50:13 +0700

    change instansi to sekolah

--- app/soal/page.tsx
+++ app/soal/page.tsx
@@ -300,11 +300,11 @@ export default function SoalPage() {
             </div>
 
             <div className="mb-2">
-              <p>Instansi asal</p>
+              <p>Sekolah asal</p>
               <input
                 className="w-full mb-3 px-4 py-2 border border-gray-300 rounded-md" 
                 type="text" 
-                placeholder="Instansi"
+                placeholder="Sekolah asal"
                 value={instansi}
                 onChange={(e) => setInstansi(e.target.value)}
               />
